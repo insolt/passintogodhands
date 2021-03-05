78966ed110ed529027b6e34a634c161ddc30b6c2
insolt <mike_born@hotmail.com>
2021-03-05 21:38:53 +0000

    Implemented new algorythm for pagination section

--- src/components/HomeAboutUs/HomeAboutUs.js
+++ src/components/HomeAboutUs/HomeAboutUs.js
@@ -7,14 +7,14 @@ const HomeAboutUs = () => {
     return(
         <section id="about-us">
             <div className="container">
-                <div class="about_us">
+                <div className="about_us">
                     <div className="about_us-msg">
                         <p className="about_us-msg_title">About Us</p>
                         <p className="about_us-msg_text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex ad quaerat dolores exercitationem dicta nobis consequuntur, sint explicabo.</p>
                     </div>
                     <div className="about_us-photo">
                         <picture>
-                            <img src={People} alt="Our team" title="Our team" loading="lazy" widtgh />
+                            <img src={People} alt="Our team" title="Our team" loading="lazy" />
                         </picture>
                     </div>
                 </div>
