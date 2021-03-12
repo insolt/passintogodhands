a054faebb54320fe3f1d2c8abe5d16d8bd8b1dc2
insolt <mike_born@hotmail.com>
2021-03-12 22:35:03 +0000

    Added logout screen

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,10 +1,36 @@
 import React from "react";
 
+import HomeLogin from "../HomeLogin";
+import HomeNav from "../HomeNav";
+import CommonButton from "../CommonComp/CommonButton";
 import "./Logout.scss";
 
 const Logout = () => {
     return(
-        <h1>Zostales wylogowany</h1>
+        <header id="logout_start">
+            <div className="container">
+                <div className="logout_header">
+                    <div className="logout_guide">
+                        <div className="logout_guide_menus">
+                            <section className="logout_login">
+                                <HomeLogin />
+                            </section>
+                            <nav className="logout_nav">
+                                <HomeNav />
+                            </nav>
+                        </div>  
+                    </div> 
+                    <div className="logout_msg">
+                        <div className="logout_msg_title">
+                            <p>You have logged out successfuly</p>
+                        </div>
+                        <div className="logout_msg_btn">
+                           <CommonButton text="Home page" to="/" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
